99596b7a0bcf7c3966094cba2d2a9a4f5775517d
j415301 <76389633+j415301@users.noreply.github.com>
2021-01-08 00:17:31 +0900

    Add files via upload

---
+++ 01_firstproject/src/com/kh/run/FirstMyObject.java
@@ -0,0 +1,15 @@
+package com.kh.run;
+
+public class FirstMyObject {
+	//자바프로젝트가 실행이 되려면 
+	//반드시 한개의 public static void main(String[] args) 메소드가 존재해야 함
+	public static void main(String[] args) {
+		//프로젝트를 실행시키면 main()메소드를 찾아서 실행한다.
+		System.out.println("안녕 Rclass 자바 화이팅!");
+		//console창에 ()안에 있는 것을 출역해라
+		//메인메소드를 실행하려면 -> main메소드가 있는 곳에서 ctrl+F11
+	
+	}
+}
+
+//ctrl+/:주석으로 처리하는 단축키
---
+++ ex_firsttest/src/com/ex/run/Test.java
@@ -0,0 +1,10 @@
+//Q. ex_firsttest프로젝트를 만들고 com.ex.run패키지에 Test클래스를 만들고 자신의 이름을 console창에 출력해보세요
+
+package com.ex.run;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("장혜린");
+	}
+
+}
